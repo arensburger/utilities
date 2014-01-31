c7fca435622f599e68c5969b8db6ce0d7ec2800e
arensburger <arensburger@gmail.com>
2014-01-31 16:11:18 +0100

    updated cleanfile.pl so it will output lines of particular length, and so that N characters may not be output

--- cleanfile.pl
+++ cleanfile.pl
@@ -8,15 +8,19 @@ use Getopt::Long;
 my $filename; #name of input file
 my $seqtype; #type of sequence
 my $outputname;
+my $length=0; #maximum length of output lines, 0 indicates that the length should not be changed 
+my $n=0; #boolean, 0 keep N's, 1 remove N's
 
 ##### read and check the inputs
 GetOptions(
 	'i:s'   => \$filename,
 	't:s'	=> \$seqtype,
+	'l:s'	=> \$length,
 	'o:s'	=> \$outputname,
+	'n:s'	=> \$n,
 );
 unless ($filename and $outputname) {
-	die "usage perl cleanfile.pl <-i file name> <-o output file, use - for standard out>  <-t OPTIONAL input type, available is \"fasta\", \"keepreturn\", default none>\n";
+	die "usage perl cleanfile.pl <-i file name> <-o output file, use - for standard out>  <-t OPTIONAL input type, available is \"fasta\", \"keepreturn\", default none> <-l OPTIONAL maximum line length in characters, default no maximum> <-n OPTIONAL\n remove N characters, default no";
 }
 open (INPUT, $filename) or die "cannot open input file $filename\n";
 if ($outputname) {
@@ -44,7 +48,19 @@ while (my $line=<INPUT>) {
 	}
 	else {
 		$line =~ s/\s//g; #remove white spaces from the current line
-		print OUTPUT $line;
+		if ($n) {
+			$line =~ s/N//ig; #remove N's
+#			$line =~ s/n//g; #remove N's
+		}
+		if ($length) { #if a maximum line length has been specified limit the output per line
+			my @line_array = ( $line =~ m/.{1,$length}/g);		
+			for (my $i=0; $i < scalar @line_array; $i++) {
+				print OUTPUT "$line_array[$i]\n";
+			}
+		}
+		else { #no maximum output line
+			print OUTPUT $line;
+		}
 		if (($seqtype eq "keepreturn") and ((length $line) > 0)) {
 			print OUTPUT "\n";
 		}
