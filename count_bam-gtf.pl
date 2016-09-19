b0f79e05dfd90bf0c13b179481ec00222d99b475
Peter Arensburger <arensburger@gmail.com>
2016-09-18 19:17:24 -0700

    updated to add rpkm calculation

--- count_bam-gtf.pl
+++ count_bam-gtf.pl
@@ -2,6 +2,7 @@
 
 # August 2013.  This script takes as input a .bam file and a gtf.  Reports the number of overlaps
 # makes use of bedtools (must be installed)
+# Sept 2016. Updated so a size of a bam file can be added and rpkm is calculated
 
 use strict;
 use File::Temp();
@@ -11,28 +12,41 @@ use Getopt::Long;
 my $bam_filename; #name of input file
 my $gtf_filename; #name of gtf
 my $outputname; #name of the output
+my $librarysize; # size of the bam file to use 
+
 GetOptions(
 	'b:s'   => \$bam_filename,
 	'g:s'	=> \$gtf_filename,
-	'o:s'	=> \$outputname
+	'o:s'	=> \$outputname,
+	't:s'	=> \$librarysize 
 );
-die ("usage: perl count_bam-gtf.pl -b <REQUIRED: input bam file> -g <REQUIRED: GTF formated file > -o <OPTIONAL: output name>\n") unless ($bam_filename and $gtf_filename);
+die ("usage: perl count_bam-gtf.pl -b <REQUIRED: input bam file> -g <REQUIRED: GTF formated file > -o <OPTIONAL: output name> -t <OPTIONAL: number of reads that should be in bam file>\n") unless ($bam_filename and $gtf_filename);
 if ($outputname) {
 	open (FINAL_OUTPUT, ">$outputname") or die ("cannot open output file $outputname");
 }
 
 # make a temporary file with only those GTF lines that are of interest
+my %transcriptlen; # name of the transcript as key and length as value
 my $gtf_short_filename = File::Temp->new( UNLINK => 1, SUFFIX => '.gtf' );
 open (INPUT, "$gtf_filename") or die "cannot open gtf file $gtf_filename\n";
 open (OUTPUT, ">$gtf_short_filename") or die "cannot create temporary file $gtf_short_filename\n";
 while (my $line = <INPUT>) {
-	if ($line =~ /three_prime_utr/) {
-		print OUTPUT $line;
-	}
-	elsif ($line =~ /five_prime_utr/) {
-		print OUTPUT $line;
-	}
-	elsif ($line =~ /\sexon\s/) {
+#	if ($line =~ /three_prime_utr/) {
+#		print OUTPUT $line;
+#	}
+#	elsif ($line =~ /five_prime_utr/) {
+#		print OUTPUT $line;
+#	}
+#	elsif ($line =~ /\sexon\s/) {
+#		print OUTPUT $line;
+#	}
+
+# update as necessary for gff file
+	if ($line =~ /\sgene\s+(\d+)\s+(\d+).+ID=(\S+);/) {
+		my $b1 = $1;
+		my $b2 = $2;
+		my $id = $3;
+		$transcriptlen{$id} = $b2 - $b1;
 		print OUTPUT $line;
 	}
 }
@@ -42,14 +56,15 @@ close OUTPUT;
 # create a file with the intersection
 my $intersect_file = File::Temp->new( UNLINK => 1, SUFFIX => '.bed' );
 `intersectBed -abam $bam_filename -b $gtf_short_filename -bed -wb > $intersect_file`;
-
+`cp $intersect_file temp.bed`;
 # summarize the intersection data
 my %uniquereads; #names of unique reads
 my %transcript_count; #transcript name as key and counts of reads intersecting as value
 my $num_intersections; #number of intersections
 open (INPUT, $intersect_file) or die "ERROR, could not open temporary file $intersect_file";
 while (my $line = <INPUT>) {
-	if ($line =~ /^\S+\s\S+\s\S+\s(\S+)\s.+transcript_id\s\"(\S+)\"/) {
+#	if ($line =~ /^\S+\s\S+\s\S+\s(\S+)\s.+transcript_id\s\"(\S+)\"/) {
+	if ($line =~ /^\S+\s\S+\s\S+\s(\S+)\s.+ID=(\S+);/) { # update as necessary for gff file
 		my $readname = $1;
 		my $transcript = $2;
 		$transcript_count{$transcript} += 1;
@@ -64,14 +79,19 @@ while (my $line = <INPUT>) {
 #output the data
 if ($outputname) {
 	foreach my $transcript (keys %transcript_count) {
-		print FINAL_OUTPUT "$transcript\t$transcript_count{$transcript}\n";
+		my $rpkm = "NA";
+		if ($librarysize) {
+			$rpkm = ((10 ** 9) * $transcript_count{$transcript})/($transcriptlen{$transcript} * $librarysize);
+		}
+		if ($outputname) {
+			print FINAL_OUTPUT "$transcript\t$rpkm\n";
+		}
+		else {
+			print "$transcript\t$transcript_count{$transcript}\t$rpkm\n";
+		}
 	}
 }
-else {
-	foreach my $transcript (keys %transcript_count) {
-                print "$transcript\t$transcript_count{$transcript}\n";
-        }
-}
+
 my $num_transcripts = keys (%transcript_count);
 my $num_reads = keys (%uniquereads);
 
