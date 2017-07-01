011506f3efe4e0138eafdc4378f8fa28c430eaba
Peter Arensburger <arensburger@gmail.com>
2017-07-01 15:39:51 -0700

    filesize.pl report number of lines for text, fasta, or fastq files

---
+++ filesize.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+use strict;
+
+use Getopt::Long;
+
+my $inputfile; 
+my $fileformat; # identifies the file format
+my $outputname; # output file
+my $number_of_lines; 
+my $filesize;
+
+GetOptions(
+	'in:s'	=> \$inputfile,
+	'f:s'   => \$fileformat,
+#	'o:s'	=> \$outputname
+);
+die ("usage: perl filesize.pl -in <REQUIRED: input file> -f <OPTIONAL: input file format: text (default), fasta, fastq>\nNote: blank and all white character lines are ignored\n") unless ($inputfile);
+if ($fileformat) {
+	die ("do not recognize file format $fileformat\n") unless (($fileformat eq "text") or ($fileformat eq "fasta") or ($fileformat eq "fastq"));
+}
+
+open (INPUT, $inputfile) or die "cannot open input file $inputfile\n";
+while (my $line = <INPUT>) {
+	if ($line =~ /\S+/) { #line must have at least one non white character
+		$number_of_lines++;
+	}
+}
+
+if ($fileformat eq "text") {
+	$filesize = $number_of_lines;
+}
+elsif ($fileformat eq "fasta") {
+	$filesize = ($number_of_lines/2);
+}
+elsif ($fileformat eq "fastq") {
+	$filesize = ($number_of_lines/4);
+}
+else {
+	$filesize = $number_of_lines;
+}
+close (INPUT);
+
+print "$filesize\t$inputfile\n";
