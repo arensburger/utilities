6edb7ce6a1892156560d4d48118e943f69574a7a
Peter Arensburger <arensburger@gmail.com>
2017-07-01 16:23:38 -0700

    updated to add options

--- combine-colums.pl
+++ combine-colums.pl
@@ -1,11 +1,22 @@
 #!/usr/bin/perl
 
+# July 2017 updated to allow options
+
 use strict;
+use Getopt::Long;
 
 my %reads;
+my $filenames; # separated by commas
+my $zeros; # add zeros in places where RPKM values are missing
+GetOptions(
+	'in:s'  => \$filenames,
+	'z'	=> \$zeros,
+);
+die ("usage: perl combine-columns.pl -in <REQUIRED: input files separated by commas> -z <OPTIONAL: put zeros where RPKMs are missing>>\n") unless ($filenames);
 
-for (my $i=0; $i < scalar @ARGV; $i++) {
-	open (INPUT, $ARGV[$i]) or die "cannot open file $ARGV[$i]\n";
+my @filenames = split (",", $filenames);
+for (my $i=0; $i < scalar @filenames; $i++) {
+	open (INPUT, $filenames[$i]) or die "cannot open file $filenames[$i]\n";
 	while (my $line = <INPUT>) {
 		my @data = split (" ", $line);
 		$reads{$data[0]}[$i] = $data[1];
@@ -15,13 +26,17 @@ for (my $i=0; $i < scalar @ARGV; $i++) {
 
 for my $name (keys %reads) {
 	print "$name";
-#	for my $i (0 .. $#{ $reads{$name} } ) {
-	for (my $i=0; $i < scalar @ARGV; $i++) {
+	for (my $i=0; $i < scalar @filenames; $i++) {
 		if (exists $reads{$name}[$i]) { 
 			print "\t$reads{$name}[$i]";
 		}
 		else {
-			print "\t0";
+			if ($zeros) {
+				print "\t0";
+			}
+			else {
+				print "\t";
+			}
 		}
 	}
 	print "\n";
