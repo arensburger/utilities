34b6759de4bc19526c2da65bf494e7b7c8ac10cf
arensburger <arensburger@gmail.com>
2017-07-03 18:46:02 -0700

    fixed to adjust for gff with no reads

--- count_bam-gtf.pl
+++ count_bam-gtf.pl
@@ -13,16 +13,14 @@ my $bam_filename; #name of input file
 my $gtf_filename; #name of gtf
 my $outputname; #name of the output
 my $librarysize; # size of the bam file to use 
-my $strand; # if set as option then only count reads in the opposite direction to gff description
-my $logtransform; # if set then log transform (base 2) the output
+my $strand; #if set as option then only count reads in the opposite direction to gff description
 
 GetOptions(
 	'b:s'   => \$bam_filename,
 	'g:s'	=> \$gtf_filename,
 	'o:s'	=> \$outputname,
 	't:s'	=> \$librarysize,
-	's'     => \$strand,
-	'l'	=> \$logtransform
+	's'     => \$strand
 );
 die ("usage: perl count_bam-gtf.pl -b <REQUIRED: input bam file> -g <REQUIRED: GTF formated file > -o <OPTIONAL: output name> -t <OPTIONAL: number of reads that should be in bam file, used for RPKM calculation> -s <OPTIONAL: only count reads that are in the opposite direction to gff annoation>\n") unless ($bam_filename and $gtf_filename);
 if ($outputname) {
@@ -59,22 +57,29 @@ close OUTPUT;
 
 # create a file with the intersection
 my $intersect_file = File::Temp->new( UNLINK => 1, SUFFIX => '.bed' );
+my $no_intersect_file = File::Temp->new( UNLINK => 1, SUFFIX => '.txt' );
+
 #print "$bam_filename\n";
 #print "$gtf_short_filename\n";
 #print "$intersect_file\n";
 if ($strand) {
-	`intersectBed -abam $bam_filename -b $gtf_short_filename -bed -wb -s > $intersect_file`;
+	`intersectBed -abam $bam_filename -b $gtf_short_filename -bed -wb -S > $intersect_file`;
+	`intersectBed -a $gtf_short_filename -b $bam_filename -S -v > $no_intersect_file`;
 }
 else {
 	`intersectBed -abam $bam_filename -b $gtf_short_filename -bed -wb > $intersect_file`;
+	`intersectBed -a $gtf_short_filename -b $bam_filename -v > $no_intersect_file`;
 }
 
 #`cp $intersect_file temp.bed`;
+#`cp $no_intersect_file no_temp.bed`;
 
 # summarize the intersection data
 my %uniquereads; #names of unique reads
 my %transcript_count; #transcript name as key and counts of reads intersecting as value
 my $num_intersections; #number of intersections
+my $num_no_intersections = 0; #number of gff elements with no intersections
+
 open (INPUT, $intersect_file) or die "ERROR, could not open temporary file $intersect_file";
 while (my $line = <INPUT>) {
 #	if ($line =~ /^\S+\s\S+\s\S+\s(\S+)\s.+transcript_id\s\"(\S+)\"/) {
@@ -89,6 +94,19 @@ while (my $line = <INPUT>) {
 	}
 	$num_intersections++;
 }
+close INPUT;
+
+#add in data from gtf entries with no intersections
+open (INPUT, $no_intersect_file) or die "ERROR, could not open temporary file $no_intersect_file";
+while (my $line = <INPUT>) {
+        if ($line =~ /ID=(\S+)/) { # update as necessary for gff file
+		$transcript_count{$1} = 0;
+	}
+        else {
+                die "cannot read no_intersect line\n$line";
+        }
+	$num_no_intersections++;
+}
 
 #output the data
 if ($outputname) {
@@ -96,10 +114,6 @@ if ($outputname) {
 		my $rpkm = "NA";
 		if ($librarysize) {
 			$rpkm = ((10 ** 9) * $transcript_count{$transcript})/($transcriptlen{$transcript} * $librarysize);
-			if ($logtransform) {
-				my $log_rpkm = log2($rpkm + 1); #add 1 to avoid problems with zeros
-				$rpkm = $log_rpkm;
-			}
 		}
 		if ($outputname) {
 			print FINAL_OUTPUT "$transcript\t$rpkm\n";
@@ -113,10 +127,5 @@ if ($outputname) {
 my $num_transcripts = keys (%transcript_count);
 my $num_reads = keys (%uniquereads);
 
-print STDERR "A total of $num_intersections intersections were found for $num_transcripts transcripts and $num_reads reads\n";
+print STDERR "A total of $num_intersections intersections were found for $num_transcripts transcripts and $num_reads reads;  $num_no_intersections elements had no intersections\n";
 close OUTPUT;
-
-sub log2 {
-	my $n = shift;
-	return log($n)/log(2);
-}
