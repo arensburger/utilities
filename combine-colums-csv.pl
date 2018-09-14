c140182a64cd63a18c7b70f28bde321f30e95c8d
arensburger <arensburger@gmail.com>
2018-09-14 12:19:00 +0200

    modified combine_columns.pl to deal with csv files

---
+++ combine-colums-csv.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+
+# January 2018 upated to report mutliple data points
+# July 2017 updated to allow options
+
+use strict;
+use Getopt::Long;
+
+my %reads;
+my $filenames; # separated by commas
+my $zeros; # add zeros in places where RPKM values are missing
+GetOptions(
+	'in:s'  => \$filenames,
+	'z'	=> \$zeros,
+);
+die ("usage: perl combine-columns.pl -in <REQUIRED: input files separated by commas> -z <OPTIONAL: put zeros where values are missing>>\n") unless ($filenames);
+
+# read the data and put results into %reads
+my @filenames = split (",", $filenames);
+for (my $i=0; $i < scalar @filenames; $i++) { # loop through the file names
+	open (INPUT, $filenames[$i]) or die "cannot open file $filenames[$i]\n";
+	while (my $line = <INPUT>) { #loop through each line of the file
+		chomp $line;
+		my @data = split (",", $line);
+		my $first_element = shift @data; 
+		my $data2string = join " ", @data;
+		$reads{$first_element}[$i] = $data2string;
+	}
+	close INPUT;
+}
+
+# report results
+
+for my $name (keys %reads) {
+	print "$name";
+	for (my $i=0; $i < scalar @filenames; $i++) {
+		if (exists $reads{$name}[$i]) { 
+			print "\t$reads{$name}[$i]";
+		}
+		else {
+			if ($zeros) {
+				print "\t0";
+			}
+			else {
+				print "\t";
+			}
+		}
+	}
+	print "\n";
+}
