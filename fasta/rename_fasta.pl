1eaf039626546daa74ffe9717b742d6f13f41616
arensburger <arensburger@gmail.com>
2018-12-03 14:34:47 +0100

    script to rename fasta

---
+++ fasta/rename_fasta.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+# December 2018 changes the names of a fasta file
+
+use strict;
+use Getopt::Long;
+
+### load data
+my $i; # input fasta file1
+my $suffix; #text to put ahead of new title
+GetOptions(
+	'in:s' => \$i,
+	's:s' => \$suffix,
+);
+die ("usage: perl rename_fasta.pl -in <REQUIRED: fasta formated file> -s <OPTIONAL: suffix, text to put as new title followed by number\n") unless ($i);
+unless ($suffix) {
+	$suffix = "seq";
+}
+
+open (INPUT, $i) or die ("Cannot open file $i\n");
+my $i=0;
+while (my $line = <INPUT>) {
+	if ($line =~ />(.+)\s$/) {
+		print ">$suffix", "$i", " ", "$1", "\n";
+	}
+	else {
+		print "$line";
+	}
+	$i++;
+}
