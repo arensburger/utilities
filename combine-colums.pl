1b189754c8019d245ed479650cb293a4022f81e6
Peter Arensburger <arensburger@gmail.com>
2019-05-03 18:24:34 -0700

    resotred old version

--- combine-colums.pl
+++ combine-colums.pl
@@ -1,19 +1,23 @@
 #!/usr/bin/perl
-# takes text files as input assuming the element of each line is refrence, and returns a file with data combined
+
 # January 2018 upated to report mutliple data points
 # July 2017 updated to allow options
 
 use strict;
 use Getopt::Long;
+use File::Basename;
 
 my %reads;
 my $filenames; # separated by commas
 my $zeros; # add zeros in places where RPKM values are missing
+my $column_number = 0; # column number to report starting with 1, if 0 then report all columns 
+
 GetOptions(
 	'in:s'  => \$filenames,
 	'z'	=> \$zeros,
+	'c:i'	=> \$column_number,
 );
-die ("usage: perl combine-columns.pl -in <REQUIRED: input files separated by commas> -z <OPTIONAL: put zeros where values are missing>>\n") unless ($filenames);
+die ("usage: perl combine-columns.pl -in <REQUIRED: input files separated by commas> -z <OPTIONAL: put zeros where values are missing> -c <OPTIONAL: column number to report starting with 1, default all columns are reported\n") unless ($filenames);
 
 # read the data and put results into %reads
 my @filenames = split (",", $filenames);
@@ -21,19 +25,39 @@ for (my $i=0; $i < scalar @filenames; $i++) { # loop through the file names
 	open (INPUT, $filenames[$i]) or die "cannot open file $filenames[$i]\n";
 	while (my $line = <INPUT>) { #loop through each line of the file
 		my @data = split (" ", $line);
-		my $first_element = shift @data;
-		my $data2string = join "\t", @data;
+		my $first_element = shift @data; 
+		
+		#decide if only one colum or al need to be stored
+		my $data2string; # holds the data to report
+		if ($column_number) {
+			$data2string = "\t$data[$column_number - 2]";
+		}
+		else {
+			$data2string = join "\t", @data;
+		}
+
+		#store the data
 		$reads{$first_element}[$i] = $data2string;
 	}
 	close INPUT;
 }
 
 # report results
+
+# print header
+print "first column";
+for (my $i=0; $i < @filenames; $i++) {
+	my $colname = basename($filenames[$i]);
+	print "\t$colname";
+}
+print "\n";
+
+# print data
 for my $name (keys %reads) {
 	print "$name";
 	for (my $i=0; $i < scalar @filenames; $i++) {
-		if (exists $reads{$name}[$i]) {
-			print "\t$reads{$name}[$i]";
+		if (exists $reads{$name}[$i]) { 
+			print "$reads{$name}[$i]";
 		}
 		else {
 			if ($zeros) {
