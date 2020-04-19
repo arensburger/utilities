0fffc7c2f46897a002f0862cfef532a93d4a354e
arensburger <arensburger@gmail.com>
2020-04-19 11:39:08 -0700

    updates

--- boxplot2.R
+++ boxplot2.R
@@ -2,12 +2,15 @@
 # this script creates multiple box plots, however, you have to specify the column names
 # by hand on either side of the tilde symbol
 # call this program from command line using
-# Rscript --vanilla /home/peter/utilities/boxplot2.R <csv file name with headers>`;
+# Rscript --vanilla /home/peter/utilities/boxplot2.R <csv file name with headers> <title> <output file name>`;
 args = commandArgs(trailingOnly=TRUE)
 data = as.data.frame(read.csv(args[1], header = TRUE))
-title = "this is the title"
-output_file_name = "temp.png"
+title = args[2]
+output_file_name =args[3]
 
-png(output_file_name)
-boxplot(coverage~chromosome,data=data, main=title, horizontal = FALSE)
+#data=as.data.frame(read.csv("~/Desktop/temp.csv"))
+
+png(output_file_name, width = 800, height = 800, units = "px")
+boxplot(data[,2]~data[,1],data=data, main=title, horizontal = TRUE)
+
 dev.off()
