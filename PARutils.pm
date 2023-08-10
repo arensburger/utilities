6e4a14355e940dc9112428c2caa561408d6a81ac
arensburger <arensburger@gmail.com>
2023-08-10 16:53:58 -0700

    removed one script that was not really used

--- PARutils.pm
+++ PARutils.pm
@@ -20,7 +20,6 @@ sub genometohash {
 	else {
 		die "ERROR, fasta file $filename does not start with a title line\n";
 	}
-
 	## dealing with the remaining lines 
 	while (my $line = <INPUT100>) {
 		if ($line =~ />(.+)/)  {
@@ -43,41 +42,6 @@ sub genometohash {
 	$genome{$title} = $seq;
 	close INPUT100;
 	return (%genome);
-}
-
-#read fasta file and return the first element title and sequence
-sub fasta_first_seq {
-	use strict;
-	(my $filename) = @_;
-	my $seq;
-	my $title;
-
-	open (INPUT100, $filename) or die "cannot open input file $filename, $!\n";
-	my $line = <INPUT100>;
-
-	# dealing with the first line
-	if ($line =~ />(.+)/) {
-		chomp $1;
-		$title = $1;
-	}
-	else {
-		return (-1, "");
-	}
-
-	# read the file through the first sequence
-	my $first_seq = 1; # boolean set to 1 until hit the next sequence starting with a greater than sign
-	while (($line = <INPUT100>) and ($first_seq)) {
-		if ($line =~ />/) {
-			$first_seq = 0;
-		}
-		else {
-			chomp $line;
-			$seq .= $line;
-		}
-	}
-
-	# return the sequences
-	return ($title, $seq);
 }
 
 #reverse complement
